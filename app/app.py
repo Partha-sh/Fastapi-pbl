9dd31c9db241634050d1f0c1e66e7cdce9fa7eac
Partha-sh <hanupartha@gmail.com>
2026-02-13 23:27:56 +0530

    Here we go with the upload the db saving of our app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,77 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,File,UploadFile,Form,Depends
 from app.schemas import PostCreate
 from app.db import Post ,create_db_and_tables,get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+# this helps us in creating the post when ever we upload a file
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session),
+):
+    
+
+#her eis the example post that we have create(session) now we will commiut it to our db
+    post  = Post(
+        caption = caption,
+        url = "dummy url"
+        ,file_type = "photo",
+        file_name = "dumm name"
+
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
+
+# this will the feed which we will be getting after uploading the file
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    # from this parameter we are getting all the post that has been created and the function explain us that we are getting the newest post
+
+    result =  await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+# result.scalars().all() can also use this
+
+
+    posts_data =[]
+    for post in posts:
+        posts_data.append({
+            "id":str(post.id),
+            "caption":post.caption,
+            "url":post.url,
+            "file_type" : post.file_type,
+            "file_name" : post.file_name,
+            "created_at": post.created_at.isoformat()
+        })
+        return {"posts": posts_data}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
